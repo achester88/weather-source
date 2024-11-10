19a61529e7dd50670f7d4e4613b80b2705ea9218
achester88 <anthonychester71@gmail.com>
2024-11-10 02:39:00 -0500

    Http call + args

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use std::collections::HashMap;
+
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    name: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+    //https://geocoding-api.open-meteo.com/v1/search?name=Berlin&count=10&language=en&format=json
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name);
+    }
+
+    let resp = reqwest::get("https://httpbin.org/ip")
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+    println!("{resp:#?}");
+    Ok(())
+}
